481f50ef5df08d74e9db3425e1d838abe70319ce
John Paul <lucas.gabule@gmail.com>
2023-06-18 23:40:04 +0800

    learn server api, middleware, handle cors headers and customer error handling

---
+++ server/api.js
@@ -0,0 +1,37 @@
+const autoCatch = require('./lib/auto-catch');
+const Products = require('./product_lists')
+
+async function listProducts(req, res) {
+	const { limit = 25, offset = 0, tag } = req.query;
+	res.json(await Products.list({
+		offset: Number(offset),
+		limit: Number(limit),
+		tag
+	}))
+}
+
+async function getProduct(req, res, next) {
+	const { id } = req.params;
+	const product = await Products.get(id)
+	if (!product) return next();
+	res.json(product)
+}
+
+async function createProduct(req, res) {
+	console.log(req.body)
+	res.json(req.body);
+}
+
+async function editProduct(req, res) {
+	console.log(req.body)
+	res.json(req.body);
+}
+
+
+async function deleteProduct(req, res) {
+	console.log(req.body)
+	res.json(req.body);
+}
+
+
+module.exports = autoCatch({ listProducts, getProduct, createProduct, editProduct, deleteProduct })
